175cec3e1a72678c14b5c0d025e1a54af37bbabd
Sandro David Gomez Sernaque <sandro_10_aries@hotmail.com>
2026-09-23 07:00:12 -0500

    Agregar archivos de proyecto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HOTEL_PEA2.Models;
+
+namespace HOTEL_PEA2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HOTEL_PEA2.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Clave { get; set; }
+
+        public string Rol { get; set; }
+
+        public int Estado { get; set; }
+    }
+}
