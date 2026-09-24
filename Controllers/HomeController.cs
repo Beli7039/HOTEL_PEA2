23aa2470e14bf0c6b13e46f760a4b05fc9683aa6
beli7039 <adelayata76@gmail.com>
2026-09-24 00:18:07 -0500

    Se creado el Dashborad

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,22 @@
-using System.Diagnostics;
-using HOTEL_PEA2.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Http;
 namespace HOTEL_PEA2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        public IActionResult Dashboard()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult DashboardContenido()
         {
-            return View();
+            return PartialView("_DashboardHome");
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Reservas()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
