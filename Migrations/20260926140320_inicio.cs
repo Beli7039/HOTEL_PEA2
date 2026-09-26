79cea153d2c233cdd63af8665b00aafeae7859b3
beli7039 <adelayata76@gmail.com>
2026-09-26 09:18:21 -0500

    cambios realizados en reserva

---
+++ Migrations/20260926140320_inicio.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HOTEL_PEA2.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    IdCliente = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Dni = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.IdCliente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recepcionista",
+                columns: table => new
+                {
+                    IdRecepcionista = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Usuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contrasena = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recepcionista", x => x.IdRecepcionista);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tipo_Habitacion",
+                columns: table => new
+                {
+                    IdTipoHabitacion = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreTipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrecioBase = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Capacidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tipo_Habitacion", x => x.IdTipoHabitacion);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Clave = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rol = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Estado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.IdUsuario);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Habitacion",
+                columns: table => new
+                {
+                    IdHabitacion = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Piso = table.Column<int>(type: "int", nullable: false),
+                    Capacidad = table.Column<int>(type: "int", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdTipoHabitacion = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habitacion", x => x.IdHabitacion);
+                    table.ForeignKey(
+                        name: "FK_Habitacion_Tipo_Habitacion_IdTipoHabitacion",
+                        column: x => x.IdTipoHabitacion,
+                        principalTable: "Tipo_Habitacion",
+                        principalColumn: "IdTipoHabitacion",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reserva",
+                columns: table => new
+                {
+                    IdReserva = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaEntrada = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaSalida = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CantidadPersonas = table.Column<int>(type: "int", nullable: false),
+                    CostoTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoHabitacion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdCliente = table.Column<int>(type: "int", nullable: false),
+                    IdHabitacion = table.Column<int>(type: "int", nullable: false),
+                    IdRecepcionista = table.Column<int>(type: "int", nullable: true),
+                    Observaciones = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HabitacionIdHabitacion = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reserva", x => x.IdReserva);
+                    table.ForeignKey(
+                        name: "FK_Reserva_Cliente_IdCliente",
+                        column: x => x.IdCliente,
+                        principalTable: "Cliente",
+                        principalColumn: "IdCliente",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reserva_Habitacion_HabitacionIdHabitacion",
+                        column: x => x.HabitacionIdHabitacion,
+                        principalTable: "Habitacion",
+                        principalColumn: "IdHabitacion");
+                    table.ForeignKey(
+                        name: "FK_Reserva_Habitacion_IdHabitacion",
+                        column: x => x.IdHabitacion,
+                        principalTable: "Habitacion",
+                        principalColumn: "IdHabitacion",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reserva_Recepcionista_IdRecepcionista",
+                        column: x => x.IdRecepcionista,
+                        principalTable: "Recepcionista",
+                        principalColumn: "IdRecepcionista",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cliente_Dni",
+                table: "Cliente",
+                column: "Dni",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Habitacion_IdTipoHabitacion",
+                table: "Habitacion",
+                column: "IdTipoHabitacion");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reserva_HabitacionIdHabitacion",
+                table: "Reserva",
+                column: "HabitacionIdHabitacion");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reserva_IdCliente",
+                table: "Reserva",
+                column: "IdCliente");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reserva_IdHabitacion",
+                table: "Reserva",
+                column: "IdHabitacion");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reserva_IdRecepcionista",
+                table: "Reserva",
+                column: "IdRecepcionista");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_UserName",
+                table: "Usuario",
+                column: "UserName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reserva");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "Habitacion");
+
+            migrationBuilder.DropTable(
+                name: "Recepcionista");
+
+            migrationBuilder.DropTable(
+                name: "Tipo_Habitacion");
+        }
+    }
+}
